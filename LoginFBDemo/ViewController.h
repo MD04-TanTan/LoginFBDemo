bde12113d76e9b9128d420a0dc2d48ac633da429
Tan Tan <hautp@TanTan.local>
2015-09-17 16:15:37 +0700

    17/9: Tan Update
    Login get user FB OK

--- LoginFBDemo/ViewController.h
+++ LoginFBDemo/ViewController.h
@@ -8,8 +8,10 @@
 
 #import <UIKit/UIKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
 @interface ViewController : UIViewController
 @property (weak, nonatomic) IBOutlet FBSDKLoginButton * loginButton;
+@property (weak, nonatomic) IBOutlet UIButton *btnLogin;
 
 @end
 
--- LoginFBDemo/ViewController.m
+++ LoginFBDemo/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 
-@interface ViewController ()
+@interface ViewController ()<FBSDKLoginButtonDelegate>
 
 @end
 
@@ -18,12 +18,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    FBSDKLoginButton * loginButton = [[FBSDKLoginButton alloc] init];
-    loginButton.center = self.view.center;
-    //loginButton.readPermissions = @[@"public_profile",@"email",@"user_friends"];
-    //[loginButton addTarget:self action:@selector(btnLoginFB:) forControlEvents:UIControlEventTouchUpInside];
+    //[self.btnLogin addTarget:self action:@selector(btnLoginFB:) forControlEvents:UIControlEventTouchUpInside];
     
-    [self.view addSubview:loginButton];
+    if ([FBSDKAccessToken currentAccessToken]) {
+        NSLog(@"Token available");
+        [self fetchUserInfo];
+    }
+    else{
+        FBSDKLoginButton * loginButton = [[FBSDKLoginButton alloc] init];
+        loginButton.center = self.view.center;
+        loginButton.readPermissions = @[@"public_profile",@"email",@"user_friends"];
+        loginButton.delegate = self;
+        [self.view addSubview:loginButton];
+    }
+}
+
+
+
+- (void)loginButton:(FBSDKLoginButton *)loginButton didCompleteWithResult:(FBSDKLoginManagerLoginResult *)result error:(NSError *)error{
+    NSLog(@"Logged in success");
+    if (error != nil) {
+        NSLog(@"%@",error);
+    }
+    [self fetchUserInfo];
+    
+}
+
+
+- (void)loginButtonDidLogOut:(FBSDKLoginButton *)loginButton{
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,8 +54,33 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)fetchUserInfo {
+    if ([FBSDKAccessToken currentAccessToken]) {
+        [[[FBSDKGraphRequest alloc] initWithGraphPath:@"me" parameters:@{@"fields": @"id, name"}]
+         startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
+             if (!error) {
+                 NSLog(@"fetched user:%@", result);
+             }
+         }];
+    }
+}
+
 - (IBAction)btnLoginFB:(id)sender{
-    
+    FBSDKLoginManager *login = [[FBSDKLoginManager alloc] init];
+    [login
+     logInWithReadPermissions: @[@"public_profile"]
+     handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
+         if (error) {
+             NSLog(@"Process error");
+         } else if (result.isCancelled) {
+             NSLog(@"Cancelled");
+         } else {
+             if ([result.grantedPermissions containsObject:@"email"]) {
+                 [self fetchUserInfo];
+             }
+             NSLog(@"Logged in");
+         }
+     }];
 }
 
 @end
